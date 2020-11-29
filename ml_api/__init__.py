f0873a0836ca3dff70d2d1d6ec2768b33768ffd0
Abdullah Selek <abdullahselek@gmail.com>
2020-11-29 19:17:21 +0000

    Start creating API

---
+++ ml_api/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""ML API for plant disease classification."""
+
+from __future__ import absolute_import
+
+__author__ = "Abdullah Selek"
+__email__ = "abdullahselek@gmail.com"
+__copyright__ = "Copyright (c) 2020 Abdullah Selek"
+__license__ = "MIT License"
+__version__ = "0.1"
+__url__ = "https://github.com/abdullahselek/ml-api"
+__download_url__ = "https://github.com/abdullahselek/ml-api"
+__description__ = "ML API for plant disease classification."
