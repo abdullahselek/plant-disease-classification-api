e09d5f0777917b32620d6439feb32d9433c068f6
Abdullah Selek <abdullahselek@gmail.com>
2020-12-20 19:25:22 +0000

    Add docs on models

--- plant-disease-classification-api/models.py
+++ plant-disease-classification-api/models.py
@@ -4,4 +4,7 @@ from pydantic import BaseModel
 
 
 class ImageItem(BaseModel):
+    """Request entity of image classification endpoint.
+       `data` is a Base64 encoded image string."""
+
     data: str
