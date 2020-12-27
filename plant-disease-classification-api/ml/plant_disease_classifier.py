5433b30b496fad0d5a6e9c403a5c9f2e99f0e5d3
Abdullah Selek <abdullahselek@gmail.com>
2020-12-27 21:20:59 +0000

    Changes on method classify

--- plant-disease-classification-api/ml/plant_disease_classifier.py
+++ plant-disease-classification-api/ml/plant_disease_classifier.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 
 from torch.autograd import Variable
 from PIL import Image
+from typing import Optional
 from plant_disease_classification.network import CNN, constant
 
 
@@ -54,15 +55,24 @@ class PlantDiseaseClassifier(object):
     def __batch_data(self, tensor):
         return tensor[None, ...]
 
-    def classify(self, image_path: str):
+    def classify(self, image_path: Optional[str], image_data: Optional[bytes]):
         """Returns a prediction class.
         Args:
-          image_path (str):
+          image_path (Optional[str]):
             Image path that is going to be used in classification.
+          image_data (Optional[bytes]):
+            Image data that is going to be used in classification.
         Returns:
           Prediction class (str)"""
 
-        tensor = self.__load_image(image_path)
+        tensor = None
+        if image_path:
+            tensor = self.__load_image(image_path=image_path)
+        elif image_data:
+            tensor = self.__load_image_from_bytes(image_data=image_data)
+        if tensor is None:
+            raise Exception("Please provide image path or data of your image!")
+
         output = self.model(self.__batch_data(tensor))
         predicted = torch.argmax(output)
         classes = constant.classes
