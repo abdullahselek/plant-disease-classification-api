34b81befb1248063b16724f65e49e54cdeab4109
Abdullah Selek <abdullahselek@gmail.com>
2020-12-28 20:16:39 +0000

    Updates on ClassficationRequestItem

--- plant_disease_classification_api/models.py
+++ plant_disease_classification_api/models.py
@@ -5,10 +5,8 @@ from pydantic import BaseModel
 
 class ClassficationRequestItem(BaseModel):
     """Request entity of classifcation item which has model path, name and image data.
-    `modelPath` is the path for model which is used for classification.
     `modelName` is the name for model which is used for classification.
     `data` is a Base64 encoded image string."""
 
-    modelPath: str
     modelName: str
     data: str
